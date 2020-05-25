518345c2d5795108a71a950073c8665f8fed3206
Hein <hgrobler1214@gmail.com>
2020-05-25 16:32:51 +1000

    my ocean code in the 'declare and add objects to the scene' section ive tweaked the ambient lighting from 0.3 to 0.55 Added a gui section for my ocean

--- src/index.js
+++ src/index.js
@@ -35,14 +35,15 @@ camera.add(cameralight);
 scene.add(camera);
 
 // Ambient Lighting
-const ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.3);
+const ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 0.55);
 scene.add(ambientLight);
 
 // Declare scene control variables here (ie: number of trees, color of leaves)
 let numberOfTrees = 1;
 let colorOfLeaves = '#2dc89b';
-let islandRotationSpeed = 0.5;
+let islandRotationSpeed = 0;
 let isDayOrNight = 'day';
+let oceanHeight = 0.14;
 
 // Scene Background
 const skyboxDay = createSkyboxDay();
@@ -66,6 +67,7 @@ const params = {
     colorOfLeaves: colorOfLeaves,
     plainRotationSpeed: islandRotationSpeed,
     isDayOrNight: isDayOrNight,
+    oceanHeight: oceanHeight,
 };
 
 // Prefer onFinishChange() to reduce re-render calls. If change is immediate use onChange()
@@ -91,6 +93,14 @@ environmentControlUI
     });
 environmentControlUI.open();
 
+const oceanControlUI = gui.addFolder('Ocean Controls');
+oceanControlUI
+    .add(params, 'oceanHeight', -1, 1)
+    .onFinishChange(val => {
+        oceanHeight = val;
+    });
+oceanControlUI.open();
+
 // Declare & Add Objects to Scene here. You can also attach objects to each other and only add the parent object to the scene
 const objLoader = new OBJLoader();
 const mtlLoader = new MTLLoader();
@@ -104,11 +114,92 @@ mtlLoader.load(islandMtlPath, materials => {
         scene.add(islandObject);
     });
 });
+    //BEGINNING HEINS CODE
+//Building the ocean
+function Ocean(){
+    //ocean mesh
+    var oceanGeom = new THREE.PlaneGeometry(500, 500, 180, 180);
+    oceanGeom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+
+    //prepare vertices for animation
+    oceanGeom.mergeVertices();
+    var l = oceanGeom.vertices.length;
+
+    //array to store ocean vertex data
+    this.waves = [];
+    for (var i = 0; i < l; i++){
+		var oV = oceanGeom.vertices[i];
+
+		//gives the vertex some info to create variation in the ocean
+		this.waves.push({y:oV.y,
+						x:oV.x,
+						z:oV.z,
+						//random angle
+						ang:Math.random()*Math.PI*2,
+						//random distance
+						amp:0.05 + Math.random()*0.2,
+						//random speed for vertexes
+						speed:0.004 + Math.random()*0.012
+					});
+	}
+
+    //ocean materials
+    var oceanMat = new THREE.MeshPhongMaterial({
+        color:0x68c3c0,
+        transparent:true,
+        opacity:0.9,
+        shading:THREE.FlatShading,
+    });
+
+    this.mesh = new THREE.Mesh(oceanGeom, oceanMat);
+
+    this.mesh.receiveShadow = true;
+
+    //function that animates the waves
+    Ocean.prototype.animWaves = function (){
+	
+        //gets vertices
+        var oceanVerts = this.mesh.geometry.vertices;
+        var len = oceanVerts.length;
+        
+        for (var i=0; i<len; i++){
+            var v = oceanVerts[i];
+            
+            //gets vertice data
+            var vertProps = this.waves[i];
+            
+            //updates vertex positions in a circular motion
+            v.x = vertProps.x + Math.cos(vertProps.ang)*vertProps.amp;
+            v.y = vertProps.y + Math.sin(vertProps.ang)*vertProps.amp;
+    
+            //increments angle
+            vertProps.ang += vertProps.speed;
+    
+        }
+
+    this.mesh.geometry.verticesNeedUpdate=true;
+
+    }
+}
+
+// Instantiating the ocean
+var ocean;
+
+function createOcean(){
+	ocean = new Ocean();
+    ocean.mesh.position.y = oceanHeight;
+	scene.add(ocean.mesh);
+}
+
+createOcean();
+    //ENDING HEINS CODE
 
 // Scene Animation (called 60 times/sec). This should call other functions that updates objects
 function animate() {
     requestAnimationFrame(animate);
     islandObject.rotateY(islandRotationSpeed / 100);
     renderer.render(scene, camera);
+    ocean.animWaves();
+    ocean.mesh.position.y = oceanHeight;
 }
 animate();
