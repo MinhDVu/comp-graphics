a4cf5926ed217fea52c07eddf8b617c68cb1055a
Monifa <monifaaam@gmail.com>
2020-05-30 00:24:09 +1000

    add snow and rain effect

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ import Ocean from './ocean';
 import islandObjPath from './models/island.obj';
 import islandMtlPath from './models/island.mtl';
 
+//import snow texture
+import snowPath from './models/snow.png';
+
+//import rain texture
+import rainPath from './models/rain.png';
+
 // Reset default CSS
 const stylesheet = document.createElement('style');
 stylesheet.type = 'text/css';
@@ -56,6 +62,13 @@ let waveSpeed = 0.08;
 let waterColor = 0x68c3c0;
 let waterOpacity = 0.5;
 let waveIntensity = 0.25;
+
+// Snow Effect Variables
+let snow, snowGeo, snowSystem;
+
+// Rain Effect Variables
+let rainSystem, rainGeo, rainCount = 30000;
+let rainDrop;
 
 // Scene Background
 const skyboxDay = createSkyboxDay();
@@ -132,6 +145,11 @@ oceanControlUI.addColor(guiParams, 'waterColor').onFinishChange(val => {
 });
 oceanControlUI.open();
 
+/* const seasonControlUI = gui.addFolder('Season Controls');
+seasonControlUI.add(guiParams, 'Rainy').onChange(GenerateSeason);
+seasonControlUI.add(guiParams, 'Winter').onChange(GenerateSeason);
+seasonControlUI.open(); */
+
 // Declare & Add Objects to Scene here. You can also attach objects to each other and only add the parent object to the scene
 const objLoader = new OBJLoader();
 const mtlLoader = new MTLLoader();
@@ -146,6 +164,120 @@ mtlLoader.load(islandMtlPath, materials => {
     });
 });
 
+//scenery
+function winter()
+{
+    snowGeo = new THREE.Geometry();
+    for (let i = 0; i < 50000; i++)
+    {
+        //set the position for the snow
+        snow = new THREE.Vector3(
+            (Math.random() - 0.5) * 200,
+            (Math.random() - 0.5) * 100,
+            (Math.random() - 0.5) * 200
+        );
+        //set the velocity
+        snow.velocity = 0.3;
+        snowGeo.vertices.push(snow);
+    }
+    //add texture to the snow
+    let snowTexture = new THREE.TextureLoader().load(snowPath);
+    let snowMaterial = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 0.7,
+        map: snowTexture,
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false
+    });
+
+    snowSystem = new THREE.Points(snowGeo, snowMaterial);
+    scene.add(snowSystem);
+    snowAnimation();
+}
+
+/*
+function rainy()
+{
+    //create rain
+    rainGeo = new THREE.Geometry();
+    for(let i=0;i<rainCount;i++) {
+        rainDrop = new THREE.Vector3(
+            (Math.random() - 0.5) * 200,
+            (Math.random() - 0.5) * 100,
+            (Math.random() - 0.5) * 200
+        );
+        rainDrop.velocity = 0.5;
+        rainDrop.velocity = 0;
+        rainGeo.vertices.push(rainDrop);
+    }
+
+    //add texture to the snow
+    let rainTexture = new THREE.TextureLoader().load(rainPath);
+    let rainMaterial = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 0.7,
+        map: rainTexture,
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false
+    });
+
+    rainSystem = new THREE.Points(rainGeo,rainMaterial);
+    scene.add(rainSystem);
+    rainAnimation();
+}
+*/
+
+//animate the snow
+function snowAnimation()
+{
+    snowGeo.vertices.forEach(p => {
+        p.x += (Math.random() - 1) * 0.1;
+        p.y += (Math.random() - 0.75) * 0.1;
+        p.z += (Math.random()) * 0.1;
+
+        //if the snow go to the max coordinate, it will restart the animation again from the specific coordinate
+        if (p.x < -100) {
+            p.x = 100;
+        }
+
+        if (p.y < -50) {
+            p.y = 50;
+        }
+
+        if (p.z < -100) {
+            p.z = 100;
+        }
+        if (p.z > 100) {
+            p.z = -100;
+        }
+    });
+    //update new position
+    snowGeo.verticesNeedUpdate = true;
+    renderer.render(scene, camera);
+    requestAnimationFrame(snowAnimation);
+}
+
+/*
+function rainAnimation()
+{
+    //animate rain
+    rainGeo.vertices.forEach(r => {
+        r.velocity -= 0.1 + Math.random() * 0.1;
+        r.y += r.velocity;
+        if (r.y < -50) {
+          r.y = 50;
+          r.velocity = 0;
+        }
+      });
+      rainGeo.verticesNeedUpdate = true;
+      rainSystem.rotation.y += 0.002;
+    renderer.render(scene, camera);
+    requestAnimationFrame(rainAnimation);
+}
+*/
+
 // Scene Animation (called 60 times/sec). This should call other functions that updates objects
 function animate() {
     fpsCounter.begin();
@@ -155,4 +287,7 @@ function animate() {
     fpsCounter.end();
     requestAnimationFrame(animate);
 }
+
 animate();
+winter();
+//rainy();
